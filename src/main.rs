1b854ee86cf98f1eec5822ded4899c327deef9eb
Jesus Gonzalez <jesus@jesusmg.net>
2025-02-20 19:56:07 -0300

    Set up wasm builds

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("akj-21")]
+async fn main() {
+    loop {
+        if is_key_pressed(macroquad::input::KeyCode::Q) {
+            miniquad::window::quit();
+        }
+
+        clear_background(RED);
+
+        draw_text("HELLO, WORLD!", 20.0, 20.0, 30.0, DARKGRAY);
+
+        next_frame().await
+    }
 }
